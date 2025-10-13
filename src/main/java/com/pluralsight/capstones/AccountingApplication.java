181fb902938e9436c3190bd9a2a0ef54a827a957
Ash <mendezashleyy@gmail.com>
2025-10-13 17:28:44 -0400

    first commit

---
+++ src/main/java/com/pluralsight/capstones/AccountingApplication.java
@@ -0,0 +1,7 @@
+package com.pluralsight.capstones;
+
+public class AccountingApplication {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/pluralsight/capstones/Transactions.java
@@ -0,0 +1,4 @@
+package com.pluralsight.capstones;
+
+public class Transactions {
+}
