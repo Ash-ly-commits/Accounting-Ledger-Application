7dfedd456cada9b0756ca1970f0195da69c706dd
Ash <mendezashleyy@gmail.com>
2025-10-13 18:47:35 -0400

    filled out Transactions class

--- src/main/java/com/pluralsight/capstones/Transactions.java
+++ src/main/java/com/pluralsight/capstones/Transactions.java
@@ -1,5 +1,40 @@
 package com.pluralsight.capstones;
 
-public class Transactions {
+import java.sql.Time;
+import java.util.Date;
 
+public class Transactions {
+    private final Date date;
+    private final Time time;
+    private final String description;
+    private final String vendor;
+    private final float amount;
+
+    public Transactions(Date date, Time time, String description, String vendor, float amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
 }
