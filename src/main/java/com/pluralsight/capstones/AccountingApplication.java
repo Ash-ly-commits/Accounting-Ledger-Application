624fb0b9b5eb559b632084e9a96113dd22f80c13
Ash <mendezashleyy@gmail.com>
2025-10-13 18:39:07 -0400

    added Home Screen Menu

--- src/main/java/com/pluralsight/capstones/AccountingApplication.java
+++ src/main/java/com/pluralsight/capstones/AccountingApplication.java
@@ -1,7 +1,36 @@
 package com.pluralsight.capstones;
+import java.util.Scanner;
 
 public class AccountingApplication {
-    public static void main(String[] args) {
+    private static final Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        String homeScreenMenu = """
+                \nHome
+                D) Add Deposit
+                P) Make Payment (Debit)
+                L) Ledger
+                X) Exit
+                Enter command:\t""";
+        char command = 'c';
+        while(!(command == 'x')) {
+            System.out.print(homeScreenMenu);
+            command = scanner.next().charAt(0);
+            switch (command) {
+                case 'D':
+                    makeDeposit();
+                    break;
+                case 'P':
+                    makePayment();
+                    break;
+                case 'L':
+                    ledgerScreen();
+                    break;
+                case 'X':
+                    System.out.println("Exiting...");
+                    break;
+            }
+        }
     }
+
 }
