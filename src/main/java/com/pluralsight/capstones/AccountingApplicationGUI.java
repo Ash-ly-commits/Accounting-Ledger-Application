8e2b10058bf860e342db24ff16732d28754221cf
Ash <mendezashleyy@gmail.com>
2025-10-17 08:44:18 -0400

    planned out my methods for AccountingApplicationGUI and added more to my css file

--- src/main/java/com/pluralsight/capstones/AccountingApplicationGUI.java
+++ src/main/java/com/pluralsight/capstones/AccountingApplicationGUI.java
@@ -1,16 +1,111 @@
 package com.pluralsight.capstones;
 
 import javafx.application.Application;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class AccountingApplicationGUI extends Application {
 
+    //I'm thinking border pane root
     public static void main(String[] args) {
-        launch(args);
+        launch();
+    }
+
+    public BorderPane createScreen(String headerTitle){
+//  BorderPane border = new BorderPane
+//  border details
+//  Label title = new Label(headerTitle)
+//  title details
+//  return border
+    }
+
+    public void createTextArea(){}
+
+    public void createButton(){}
+
+    public void showAlert(){}
+
+    public void homeScreen(){
+//  border plan root
+//  deposit button -> setaction makeTransactionScreen("d")
+//  payment button -> setaction makeTransactionScreen("p")
+//  ledger button -> setaction ledgerScreen()
+//  exit button -> setaction system exit
+    }
+
+    public void makeTransactionScreen(String option){
+//  border pane root
+//  "Enter transaction info. " label on top
+//  description label on left, text field on right for response
+//  vendor label on left, text field on right for response
+//  amount label on left, text field on right for response
+//  submit button bottom right -> setaction try{ getText from fields & call makeTransaction(option, description,
+//  vendor, amount) } catch (Exception e) {showAlert() }
+//  if try goes through fine, setText("Transaction added") & then back to homeScreen()
+    }
+
+    public void ledgerScreen(){
+//  border pane root
+//  top HBox of buttons below:
+//  Home button top left -> setaction homeScreen()
+//  Reports button top right -> setaction reportsScreen()
+//  display text area center
+//  bottom HBox of buttons below:
+//  Display All button -> setaction displayLedger(text area box thing, predicate all)
+//  Display Deposits button -> setaction displayLedger(text area box thing, predicate deposits)
+//  Display Payments button -> setaction displayLedger(text area box thing, predicate payments)
+    }
+
+    public void reportsScreen(){
+//  border pane root
+//  top HBox of buttons below:
+//  Ledger button top left -> setaction ledgerScreen()
+//  Search by Vendor button top middle -> setaction vendorScreen()
+//  Custom Search button top right -> setaction customSearchScreen()
+//  display text area center
+//  bottom HBox of buttons below:
+//  Display Month to date button -> setaction displayLedger(text area box thing, predicate)
+//  Display Previous month button -> setaction displayLedger(text area box thing, predicate)
+//  Display Month to year button -> setaction displayLedger(text area box thing, predicate)
+//  Display Month to year button -> setaction displayLedger(text area box thing, predicate)
+//  Display Previous year button -> setaction displayLedger(text area box thing, predicate)
+    }
+
+    public void vendorScreen(){
+//  border pane root
+//  Reports button top left -> setaction reportsScreen()
+
+//  display text area center
+
+//  bottom Hbox of buttons below:
+//  vendor label on bottom left, text field in bottom middle for response
+//  submit button bottom right -> setaction try{ getText from field & displayLedger(text area box, predicate)
+    }
+
+    public void customSearchScreen(){
+//  border pane root
+//  top Hbox with stuff below:
+//  Reports button left -> setaction reportsScreen()
+//  "Custom Search" label middle
+
+//  Center Vbox maybe:
+//  setText("Enter only the values you want to filter by") top
+//  start date label on left, text field on right for response
+//  end date label on left, text field on right for response
+//  description label on left, text field on right for response
+//  vendor label on left, text field on right for response
+//  amount label on left, text field on right for response
+//  submit button -> setaction try{ getText from fields & customSearch(text area box, start, end, description,
+//  vendor, amount) } catch (Exception e) {showAlert() }
+
+//  bottom text area box
     }
 
     @Override
     public void start(Stage primaryStage) {
-
+//  set screen container and scene to container
+//  add css to scene
+//  set stage to scene
+//  primaryStage.show()
     }
 }
