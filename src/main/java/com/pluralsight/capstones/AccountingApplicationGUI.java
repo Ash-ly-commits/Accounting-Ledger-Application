8967a86f40947bffb31cd9ca3ed88b12d306d05a
Ash <mendezashleyy@gmail.com>
2025-10-17 07:52:50 -0400

    added a javafx application (AccountingApplicationGUI class) and made sure javafx was enabled and installed

---
+++ src/main/java/com/pluralsight/capstones/AccountingApplicationGUI.java
@@ -0,0 +1,16 @@
+package com.pluralsight.capstones;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AccountingApplicationGUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
